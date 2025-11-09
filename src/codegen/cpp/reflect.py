0cd95628a57afb241340cd542b666f6bedeb33ef
Ismael Bueno <ismaelwno@gmail.com>
2025-11-09 13:18:13 +0100

    chore: Replace error with fatal

--- src/codegen/cpp/reflect.py
+++ src/codegen/cpp/reflect.py
@@ -87,7 +87,7 @@ def generate_reflection_code(hpp: CPPGenerator, classes: ClassCollection, /) -> 
                         with hpp.scope(f'if (p_Value == "{val}")', delimiters=False):
                             hpp(f"return {enum.id.identifier}::{val};")
 
-                    hpp('TKIT_ERROR("Found no valid enum value for the string {}.", p_Value);')
+                    hpp('TKIT_FATAL("Found no valid enum value for the string {}.", p_Value);')
                     hpp(f"return {enum.id.identifier}::{vals[0]};")
 
                 with hpp.doc():
